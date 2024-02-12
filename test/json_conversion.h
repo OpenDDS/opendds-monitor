a953f77cb0b068f08df3ea3f0a2a2a16d7a86b5a
Fred Hornsey <hornseyf@unityfoundation.io>
2024-02-12 12:43:32 -0600

    Respond to Review

--- test/json_conversion.h
+++ test/json_conversion.h
@@ -1,25 +1,18 @@
 #pragma once
 
-#include <iostream>
-
 #include <dds/DCPS/RapidJsonWrapper.h>
-
 #ifndef OPENDDS_RAPIDJSON
 #  define OPENDDS_RAPIDJSON
 #endif
-
 #include <dds/DCPS/JsonValueWriter.h>
 #include <dds/DCPS/JsonValueReader.h>
 
-//template<typename IDL_Type>
-//bool json_2_idl(std::istream& is, IDL_Type& idl_value) {
-//  rapidjson::IStreamWrapper isw(is);
-//  return OpenDDS::DCPS::from_json(idl_value, isw);
-//}
+#include <iostream>
 
 template<typename IDL_Type, typename Writer_Type = rapidjson::Writer<rapidjson::OStreamWrapper> >
 bool idl_2_json(const IDL_Type& idl_value, std::ostream& os,
-    int max_decimal_places = Writer_Type::kDefaultMaxDecimalPlaces) {
+    int max_decimal_places = Writer_Type::kDefaultMaxDecimalPlaces)
+{
   rapidjson::OStreamWrapper osw(os);
   Writer_Type writer(osw);
   OpenDDS::DCPS::JsonValueWriter<Writer_Type> jvw(writer);
